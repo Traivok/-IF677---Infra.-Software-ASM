8c3806ed66b41c6eb6467cb8336ab0dc92d09ac1
Traivok <jraf@cin.ufpe.br>
2017-08-18 16:57:32 -0300

    Read, Print, Readv

--- IO.asm
+++ IO.asm
@@ -1,58 +1,104 @@
 org 0x7c00
 jmp 0x0000:start
 
-num1 db '000', 0
-num2 db '000', 0
+num1 db 0
+num2 db 0
 
 start:
-	xor ax, ax ;zera ax
-	mov ds, ax
-	mov es, ax
+	xor ax, ax		; init
+	mov ds, ax 		; init
+	mov es, ax 		; init
 
-	mov di, num1 ;; pass num1 as arg to read
-	call readstr ;; call read(num1)
+	mov di, num1 		; pass num1 as arg to read	
+	call readstr 		; call read(num1)
 	
 	mov si, num1
 	call printstr
-
+	call println
+	
 	jmp done
 
-printstr:	
+;;; print string from si
+printstr:
 	.start:
 
-		lodsb ;; si -> al
+		lodsb 		; si -> al
 		cmp al, 0
-		je .done ;; if (end of string) return
-		jmp .print ;; else print current char
+		je .done 	; if (end of string) return
+		jmp .print 	; else print current char
 
 		.print:
-			mov ah, 0xe ;; print char and move cursor foward
-			mov bh, 0 ;; page number
-			mov bl, 0xf ;; white color
-			int 10h ;; video interrupt
+			mov ah, 0xe 	; print char and move cursor foward
+			mov bh, 0 	; page number
+			mov bl, 0xf 	; white color
+			int 10h 	; video interrupt
 
-			jmp .start
-
+			jmp .start 
 		.done:
 			ret
 
-print_new_line:
-	mov ah, 0xe ;; char print
-	mov bh, 0 ;; page number
-	mov bl, 0xf ;; white color
-	mov al, 13 ;; vertical tab
-	int 10h ;; visual interrupt
+;;; read string from di
+readstr:
+	.read:
+		mov ah, 0 	; read keystroke
+		int 16h		; keyboard interrupt
+
+		cmp al, 0xd 	; compare al with 'enter'
+		je .done
 	
-	mov ah, 0xe ;; char print
-	mov bh, 0 ;; page number
-	mov bl, 0xf ;; white color
-	mov al, 10 ;; backspace
-	int 10h ;; visual interrupt	
+		stosb
+		jmp .read	
+	
+	.done:
+		mov al, 0 	; insert '\0'
+		stosb
+		ret
+
+;;; read (verbosely) string from di and print char by char
+readvstr:		
+	.read:
+		mov ah, 0 	; read keystroke
+		int 16h		; keyboard interrupt
+
+		cmp al, 0xd 	; compare al with 'enter'
+		je .done
+	
+		stosb
+		jmp .print
+	
+	.print:
+		mov ah, 0xe 	; call number
+		mov bh, 0	; page number
+		mov bl, 0xf	; white color
+		int 10h
+
+		jmp .read
+
+	.done:
+		call println 	; print line
+		mov al, 0 	; insert '\0'
+		stosb
+	
+		ret 		; return
+	
+;;; print line (\n)
+println:
+	mov ah, 0xe ; char print
+	mov bh, 0 ; page number
+	mov bl, 0xf ; white color
+	mov al, 13 ; vertical tab
+	int 10h ; visual interrupt
+	
+	mov ah, 0xe ; char print
+	mov bh, 0 ; page number
+	mov bl, 0xf ; white color
+	mov al, 10 ; backspace
+	int 10h ; visual interrupt	
 
 	ret
 
 done:
-	jmp $
+	jmp $ 			; infinity jump
 
 times 510-($-$$) db 0   ; Pad remainder of boot sector with 0s
 dw 0xAA55               ; The standard PC boot signature
