269778bd2a94c2a7f7e0ef35c06337eb0a65d15f
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2017-08-20 02:51:34 -0300

    está printando ao contrário

--- to_string.asm
+++ to_string.asm
@@ -1,17 +1,17 @@
 org 0x7c00
 jmp 0x0000:start
 
-array_int db 157, 151, 122, 77, 66, 0
+array_int db 157, 151, 122, 77, 0
 array_string times 12 db 0 ;a quantidade de caracteres do array int
 
 start:
 	xor ax, ax		; ax = 0
 	mov ds, ax 		; ds = 0
 	mov es, ax 		; es = 0
+	mov si, array_int
+	mov di, array_string
+	lodsb ; si -> al
 
-mov si, array_int
-mov di, array_string
-lodsb ; si -> al
 to_string:
 
 	.extract_transform: ; extrai o último algarismo de ax
@@ -23,13 +23,6 @@ to_string:
 		stosb ; al -> di
 
 		mov dh, ah ; guarda o valor de ah
-		; operação de print do character contido em al
-		mov ah, 0xe ; número da chamada
-		mov bh, 0 ; número da página
-		mov bl, 0xf ; cor da letra, branco
-		int 10h
-		; terminando a operação de print
-
 		xor ax, ax ; ax = 0
 		mov al, dh ; al recebe o antigo valor de ah
 		cmp al, 0 ; (al == 0) -> número todo transformado
@@ -39,6 +32,17 @@ to_string:
 	lodsb ; si -> al
 	cmp al, 0 ; checa se chegou no fim do array_int
 	jne to_string
+
+	mov si, array_string ; para ser usado na print_string
+
+print_string:
+	lodsb ; si -> al
+	mov ah, 0xe ; número da chamada
+	mov bh, 0 ; número da página
+	mov bl, 0xf ; cor da letra, branco
+	int 10h
+	cmp al, 0
+	jne print_string
 
 done:
 	jmp $
