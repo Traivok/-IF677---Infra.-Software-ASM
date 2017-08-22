cd3e9b990c19ca310718adbccfda3f123e00100f
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2017-08-22 11:32:50 -0300

    Add files via upload

---
+++ 3.asm
@@ -0,0 +1,198 @@
+org 0x7c00
+jmp 0x0000:start
+
+msg1 db "String Inverter", 13, 10, 0
+msg2 db "Put the string you want to invert: ", 0
+string times 100 db 0
+inverted_string times 100 db 0
+	
+start:
+	xor ax, ax		; ax = 0
+	mov ds, ax 		; ds = 0
+	mov es, ax 		; es = 0
+	mov ss, ax		; stack init
+	mov sp, 0x7c00		; stack init
+	
+	mov si, msg1
+	call printstr
+	mov si, msg2
+	call printstr
+
+	; reading the string from the user
+	mov di, string
+	call readvstr
+	;;
+
+	mov si, string
+	mov di, inverted_string
+	call str_inverter
+
+	mov si, inverted_string
+	call printstr
+
+;;; inverts the string 
+;; @param use si as the string input
+;; @return di as the inverted string output
+;; @reg: ax, si, di
+str_inverter:
+
+	push 0 ; '\0' end of string
+
+.stack:
+
+	xor ax, ax ; ax = 0
+	lodsb ; si -> al
+	cmp al, 0
+	je .inverter ; can't put the 0 into the stack
+	push ax ; else put ax into the stack
+	jmp .stack 
+
+.inverter: 
+
+	pop ax
+	stosb ; al -> di
+	cmp al, 0 ; end of the string
+	je .done
+	jmp .inverter
+
+.done:
+
+	ret
+
+;;; integer to string -- string	to_string(int*)
+;; @param use ax as number input
+;; @return di as string output
+;; @reg: ax, bl, sp, di
+tostring:
+	
+	push 0 			; push '\0' at the end of the string
+
+.convert:			; convert every digit of integer input into characters
+	
+	mov bl, 10		; let number = 123, then, after div, 12 will be al, and 3 will be ah
+	div bl			; so, we need to push 3 onto stack and recursively convert (number/10) until the result be zero 
+	add ah, '0'		; convert remainder to ascii...
+
+	mov dl, ah		; (although the remainder is stored to ah, the stosb works with al)
+	push dx			; ...and push it	
+
+	cmp al, 0		; base case condition
+	je .concat
+	
+	mov ah, 0		; the remainder was pushed onto stack, we dont need it anymore so AX = [3, 12] -> [0, 12]
+	jmp .convert
+	
+.concat:			; concat every char of stack into a string
+	
+	pop ax			; get top of stack and pop it
+	
+	stosb			; store al at di
+	
+	cmp al, 0 		; if end of string
+	je .done		; goto done
+	jmp .concat
+	
+.done:
+	ret
+	
+;;; print string
+;; @param: use si to print
+;; @reg: ax, bx
+printstr:
+	.start:
+
+		lodsb 		; si -> al
+		cmp al, 0
+		je .done 	; if (end of string) return
+		jmp .print 	; else print current char
+
+		.print:
+			mov ah, 0xe 	; print char and move cursor foward
+			mov bh, 0 	; page number
+			mov bl, 0xf 	; white color
+			int 10h 	; video interrupt
+
+			jmp .start 
+		.done:
+			ret
+
+;;; read (verbosely) string from di and print char by char
+;; @param: use di to read 	
+;; @reg: ax, bx
+readvstr:		
+	.read:
+		mov ah, 0 	; read keystroke
+		int 16h		; keyboard interrupt
+
+		cmp al, 0xd 	; compare al with 'enter'
+		je .done
+	
+		stosb
+		jmp .print
+	
+	.print:
+		mov ah, 0xe 	; call number
+		mov bh, 0	; page number
+		mov bl, 0xf	; white color
+		int 10h
+
+		jmp .read
+
+	.done:
+		call println 	; print line
+		mov al, 0 	; insert '\0'
+		stosb
+	
+		ret 		; return
+	
+;;; print line (\n)
+;; @reg: ax, bx
+println:
+	mov ah, 0xe ; char print
+	mov bh, 0 ; page number
+	mov bl, 0xf ; white color
+	mov al, 13 ; vertical tab
+	int 10h ; visual interrupt
+	
+	mov ah, 0xe ; char print
+	mov bh, 0 ; page number
+	mov bl, 0xf ; white color
+	mov al, 10 ; backspace
+	int 10h ; visual interrupt	
+
+	ret
+
+;;; string to integer -- int atoi(string*) 
+;; @param use si as string
+;; @return dl as int result
+;; @reg: ax, dx, bl, si
+atoi:
+	xor ax, ax 		; init
+	mov dx, ax
+.convert:	
+	lodsb
+
+	cmp al, '0'
+	jb .done 		; character below '0'
+	
+	cmp al, '9'
+	ja .done		; character above '9'
+
+	sub al, '0'		; convert ascii to (0-9) int
+
+	xchg dl, al 		; this swap is needed because mul
+
+	mov bl, 10		; supose 12 from 123 string was computed, then 123 = (12*10) + 3
+	mul bl			; prepare data for next unit digit
+	add dl, al		; insert new digit into data
+	
+	jmp .convert
+	
+.done:
+	ret	
+	
+done:
+	jmp $ 			; infinity jump
+
+times 510-($-$$) db 0   ; Pad remainder of boot sector with 0s
+dw 0xaa55               ; The standard PC boot signature
